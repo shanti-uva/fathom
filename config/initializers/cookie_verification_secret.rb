847476055475d4f51b82c185b39c56b21766dbe6
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2012-09-03 09:45:19 -0600

    removed unused files

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f02539dc0d6d59ace42558fa7e09ea3e7d3c9801cd94a0ad580f6a6ef6de5bd7ed40cf69a475e12c343b3c53101be657306e218a58c0115daf51ec6274dd841b';
